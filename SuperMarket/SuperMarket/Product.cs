079414375278f4aae6398c8632b25b7e6c11b6f0
DEI ROMERO <DEI ROMERO@DESKTOP-UNSED2L>
2022-03-01 18:03:03 -0500

    Finish Class Product

---
+++ SuperMarket/SuperMarket/Product.cs
@@ -0,0 +1,21 @@
+﻿namespace SuperMarket
+{
+    public abstract class Product
+    {
+        public string Description { get; set; }
+        public int Id { get; set; }
+        public decimal Price { get; set; }
+        public float Tax { get; set; }
+
+        public abstract decimal GetValeuToPay();
+
+        public override string ToString()
+        {
+            return $"  {Id}  {$"{Description}",13}" +
+                    $"\n\tPrice.......:{$"{Price:C2}",14}" +
+                    $"\n\tTax.........:{$"{Tax:P2}",16}";
+        }
+
+
+    }
+}
